3e672091983143abe994025bed404a50c4720842
Carlos Andres Estrada Vasquez <121978106+carlosestradacitytech@users.noreply.github.com>
2023-06-05 20:48:51 -0500

    Codigo

--- Datos Primitivos/Ejercicio_2.js
+++
@@ -1,5 +0,0 @@
-// Ejercicio 2: Verificación de tipo de dato
-// En este ejercicio, se solicita al usuario que ingrese un valor y se muestra en la consola el
-// tipo de dato utilizando el operador typeof.
-
-//typeof();//Ingresar dentro del typeOf lo que se requiera
