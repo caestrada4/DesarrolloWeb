6f53d7dce0ee724e397890a6ce825d9d0bdc5b50
Carlos Andres Estrada Vasquez <121978106+carlosestradacitytech@users.noreply.github.com>
2023-06-07 00:03:43 -0500

    Cambio

---
+++ query2.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE tarea_001;
+
+USE tarea_001;
+
+CREATE TABLE usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL
+);
