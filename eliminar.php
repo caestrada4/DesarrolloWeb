02ad8b1ebcc3695436d24ba670cb961ec8b5dd1d
Carlos Andres Estrada Vasquez <49221016+caestrada4@users.noreply.github.com>
2023-06-07 00:41:17 -0500

    Cambios

--- eliminar.php
+++ eliminar.php
@@ -7,7 +7,7 @@ $contacto_id = $_POST['contacto_id'];
 $sql = "DELETE FROM contactos WHERE id = $contacto_id";
 
 if ($conn->query($sql) === TRUE) {
-    echo "Contacto eliminado correctamente.";
+    echo "Contacto eliminado correctamente. <a href='index.php'>Regresar a la lista de contactos</a>";
 } else {
     echo "Error al eliminar el contacto: " . $conn->error;
 }
